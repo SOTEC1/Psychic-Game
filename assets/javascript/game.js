fd4e7f9c21070b67765a6b169ff5af9913b7e6f4
Cotes <cotes.brandon@yahoo.com>
2018-12-12 21:24:28 -0500

    more progess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // computer choices array
 var compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+console.log(compChoice);
 // wins/losses variables
 var chances = 10;
 var wins = 0;
@@ -12,8 +12,25 @@ var lossesText = document.getElementById("lossesText");
 var chancesText = document.getElementById("chancesText");
 
 // player chooses letter to guess
+document.onkeyup = function(event) {
+  var playerChoice = event.key;
 
-// if player guesses correct +1 win
+  var compGuess = compChoice[Math.floor(Math.random() * compChoice.length)];
+
+// if player player guesses correct wins +1
+if ((playerChoice === compGuess)) {
+  wins++;
+}
+else {
+  losses++;
+}
+
+winsText.textContent = "wins: " + wins;
+lossesText.textContent = "losses: " + losses;
+
+};
+
+
 // if player guesses wrong -1 chances
 // computer logs/prints wrong answers
 // when chances run out loss +1
