a734cf9888242362f213da74c7e2fd2bd15ee12d
Cotes <cotes.brandon@yahoo.com>
2018-12-13 20:28:00 -0500

    game done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,51 +4,57 @@ var compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 
 // wins/losses variables
 var chances = [];
-var chancesLeft = 9;
+var chancesLeft = 10;
 var wins = 0;
 var losses = 0;
+var compGuess;
+var lettersGuessed = [];
 
-// reset function
-function reset () {
-  chancesLeft = 9;
 
-  chancesFar = [];
-  compGuess = compChoice [Math.floor(Math.random() * compChoice.length)];
-}
 
-reset();
-
-// letters guessed
+// reset
 function newword () {
   chances = [];
-  chancesLeft = 9;
-  computerGuess = computerChoice[Math.floor(Math.random() * compChoice.length)];
+  chancesLeft = 10;
+  compGuess = compChoice[Math.floor(Math.random() * compChoice.length)];
+  console.log(compGuess);
 }
-
+newword()
 // prints game components to screen 
 var winsText = document.getElementById("winsText");
 var lossesText = document.getElementById("lossesText");
 var chancesText = document.getElementById("chancesText");
+var lettersGuessed = document.getElementById("guessedText");
 
 // player chooses letter to guess
 document.onkeyup = function(event) {
   // 
   var playerChoice = event.key;
-
+  
 // making computer chose from array 
-  var compGuess = compChoice[Math.floor(Math.random() * compChoice.length)];
 
 // if player guesses correct wins +1
+
 if ((playerChoice === compGuess)) {
   wins++;
+  alert("You won!")
+  newword();
 }
+
+
 // if play guesses wrong losses +1
 if ((playerChoice !== compGuess)) {
-  losses++
+  chancesLeft--;
+  
+  if ((chancesLeft === 0)) {
+    losses++;
+    newword();
+  }
 }
 
 // prints wins/losses
 winsText.textContent = "wins: " + wins;
 lossesText.textContent = "losses: " + losses;
-chancesText.textContent = "Chances: " + chances;
+chancesText.textContent = "Chances: " + chancesLeft;
+guessedText.textContent = "Letters chosen: " + playerChoice;
 };
